03cc954a0e741c1f6ca4771152e8150c12af4f06
Сергей Зеленский <61405650+Wolfgang-Zipper@users.noreply.github.com>
2023-03-20 14:31:03 +0000

    Оптимизирован код, доработан offset, добавлена обработка ошибок от API

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 let fetch = require('node-fetch');
 global.Headers = fetch.Headers;
-
 let url = "https://ratingcp.helpdeskeddy.com/"
-const token = "";
+const token = "cy56ZWxlbnNraXlAY2xvdWRwYXltZW50cy5ydTpiMzQ0YzJjZC03ZmJlLTRmOTItYTJhZi04ZGU3MDA1Y2U2Njg=";
 let requests = 0;
-let response_objects = []; // массив для подсчета объектов ответа API
-async function reqHde(offset, endpoint, param) {
 
+
+async function reqHde(endpoint, param, offset = 1) {
   try {
-    await fetch(`${url}api/v2/${endpoint}/?page=${offset}${param}`, { // запрос к API с параметрами
+    const response_objects = [];
+    const response = await fetch(`${url}api/v2/${endpoint}/?page=${offset}${param}`, {
       method: "GET",
       headers: {
         "Authorization": `Basic ${token}`,
       }
-    })
-      .then(response => response.json()) // получаем ответ в формате JSON
-      .then((r) => {
+    });
+    if (response.ok) { // Проверяем, был ли запрос успешным
+      const r = await response.json();
+      for (let key in r.data) {
+        response_objects.push(r.data[key]);
+      }
+      if (r.pagination.total_pages > offset) {
+        return response_objects.concat(await reqHde(endpoint, param, offset + 1));
+      } else {
+        return response_objects;
+      }
+    }
 
-        for (let key in r.data) {
-          response_objects.push(r.data[key]); // заносим каждый массив в объект
+    else {
+      throw new Error(`Ошибка запроса. Ответ сервера: ${response.status}`);
 
-        }
-
-        if (r.pagination.total_pages > offset) { // решение страниц пока все страницы не будут обработаны
-          return reqHde(offset + 1, endpoint, param);
-        } else {
-          console.log(response_objects); // вывод всех объектов на экран
-        }
-      });
+    }
   } catch (err) {
-    console.log(err); // вывод ошибки, если запрос не выполнится успешно
+    console.log(err);
   }
 }
 
-reqHde(1, "tickets", "&search=John") // вызов функции с определенными параметрами
+
+reqHde("tickets", "&status_list=open,closed")
+  .then(response => console.log(response.length))
+  .catch(error => console.error(error));
+
+reqHde("users")
+  .then(response => console.log(response))
+  .catch(error => console.error(error));
+
